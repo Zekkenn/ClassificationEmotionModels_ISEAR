7fe1a45853035f927d11f676ee76739db66f0ad3
JEMurcia <jossie.murcia@hotmail.com>
2018-09-14 01:31:15 -0500

    Exploración

--- R code/data_exploration.R
+++ R code/data_exploration.R
@@ -1,13 +1,81 @@
-# Data Exploration
+library(tidyverse)
+library(tokenizers)
+library(plotly)
 
-library("plotly")
+# =============================== EXPLORATORY ANALYSIS ========================================
 
-# Receives a vector labels... [joy, joy, fear, joy, anger, ...]
+# GENERAL DATA EXPLORATION
+#   sentences: chr vector with speeches
+#   emotions: factor with emotion label for every sentence
+explore.data <- function(sentences,emotions){
+  print(str(sentences))
+  print(str(emotions))
+  
+  # ANALYSIS OF WORDS ON SENTENCES
+  write("WORDS ON SENTENCES",stdout())
+  words <- tokenize_words(sentences)
+  visualize.nwords(words,emotions,"words")
+  
+  # ANALYSIS OF PHRASES ON SENTENCES
+  write("PHRASES ON SENTENCES",stdout())
+  phrases <- tokenize_sentences(sentences)
+  visualize.nwords(phrases,emotions,"phrases")
+  
+  # EMOTION FRECUENCIES
+  visualize_data(emotions)
+  
+  # WORDCLOUD
+  generate.wordcloud(sentences)
+}
+
+
+# HISTOGRAM OF EMOTIONS FRECUENCIES 
+#   Receives a vector labels... [joy, joy, fear, joy, anger, ...]
 visualize_data <- function(test){
   test_frame <- subset(as.data.frame(table(test)), Freq != 0)
   p <- plot_ly(test_frame, x = test_frame$test, y = test_frame$Freq, type = 'bar') %>%
-    layout(title = "Distribución de Sentimientos", 
-           xaxis = list(title = "Frecuencia de Aparición"), 
-           yaxis = list(title = "Sentimientos"))
+    layout(title = "Distribuci�n de Emociones", 
+           xaxis = list(title = "Frecuencia de Apariciones"), 
+           yaxis = list(title = "Emociones"))
   print(p)
 }
+
+
+# SCATTER OF WORDS OR PHRASES IN PHRASES
+visualize.nwords <- function(words,emots,part.of.whole = "words"){
+  total.words <- plot_ly(x = rep(1:length(words)), y = sapply(words, length), name = "Word counts", type = "scatter", mode = "lines") %>%
+    add_trace(y = ~mean(sapply(words, length)), name = paste('Mean: ', mean(sapply(words, length))), line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dash')) %>%
+    add_trace(y = ~min(sapply(words, length)), name = paste('Min: ', min(sapply(words, length))), line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dot')) %>%
+    add_trace(y = ~max(sapply(words, length)), name = paste('Max: ', max(sapply(words, length))), line = list(color = 'rgb(205, 12, 24)', width = 4)) %>%
+    layout(title = paste(part.of.whole,'in Sentences'),
+           xaxis = list(title = 'sentence'),
+           yaxis = list (title = paste('number of',part.of.whole)))
+  print(total.words)
+  
+  nwords.byEmot <- plot_ly(ggplot2::diamonds, y = sapply(words, length), color = emots, type = "box")
+  print(tapply(sapply(words, length), emots, summary))
+  print(nwords.byEmot)
+}
+
+
+# WORDCLOUD : MOST FREC WORDS
+generate.wordcloud <- function(sentences, data.title = "Most Used Words"){
+  
+  # FRECUENCY MATRIX OF WORDS
+  data.docs <- Corpus(VectorSource(sentences))
+  dtm <- TermDocumentMatrix(data.docs)
+  data.freq <- as.matrix(dtm)
+  data.freq <- sort(rowSums(data.freq),decreasing=TRUE)
+  data.freq <- data.frame(word = names(data.freq),freq=data.freq)
+  
+  # GENERATE WORDCLOUD
+  
+  par(bg="grey30")
+  png(file="WordCloud.png",width=1000,height=700, bg="grey30")
+  write("Generating wordCloud .....", stdout())
+  wordcloud(data.freq$word, data.freq$freq, col=terrain.colors(length(data.freq$word), alpha=0.9), random.order=FALSE, rot.per=0.3 )
+  write("Done, Saved on Working Directory", stdout())
+  title(main = data.title, font.main = 1, col.main = "cornsilk3", cex.main = 1.5)
+  dev.off()
+}
+
--- R code/data_loader.R
+++ R code/data_loader.R
@@ -1,25 +1,87 @@
-#setwd("/home/suntse/Documents/Universidad/trab_dir/ClassificationEmotionModels_ISEAR")
-
-file <- "py_isear_dataset/isear.csv"
-#mat <- read.csv(file, header = TRUE, sep = "|", dec = ".")
-
 library(qdap)
 library(tm)
 library(SnowballC)
 library(wordcloud)
 library(RColorBrewer)
 library(caret)
-library(readr)
+#library(readr)
 
-# DATA MAIN
+# ================================ DATA MAIN ==============================================
 
-# get_all_data <- function(){
-#   return( mat )
-# }
-# 
-# get_data <- function(labels_data){
-#   return( mat[,labels_data] )
-# }
+# DATA TO OBTAIN
+getData.ISEAR <- function(path){
+  
+  # READ & GET IMPORTANT ATTRIBUTES
+  data <- read.csv(path, header = TRUE, sep = "|", stringsAsFactors = FALSE)
+  data <- data[c("COUN","SUBJ","AGE","EMOT","Field1","SIT","LONG","INTS")]
+  
+  # EMOTIONS TAGGED
+  data <- data[(data$Field1 != ""),]
+  data$EMOT <- factor(data$EMOT)
+  levels(data$EMOT) <- list("joy" = "1", "fear" = "2", "anger" = "3", "sadness" = "4", "disgust" = "5", "shame" = "6", "guilt" = "7")
+  
+  return(data)
+}
+
+
+# GET ALL DATA WITHOUT NOISE
+#   EMOT label -> emotion tag
+#   SIT label -> Text
+getPreproc.Data.ISEAR <- function(path = ""){
+  
+  if (path == "") {
+    path <- paste(getwd(),"isear.csv", sep = "/")
+  }
+  data.raw <- getData.ISEAR(path)
+  data.prep <- preproccess.ISEAR(data.raw)
+  
+  return(data.prep)
+}
+
+
+# DATA PRE_PROCESS
+preproccess.ISEAR <- function(isear.data){
+  
+  # DELETE ALL NON-ALPHANUMERIC CHARACTERS & additional whitespace
+  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub("[^a-zA-Z0-9']", " ", x)) # non-alphanumeric characters
+  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub("\\s+", " ", x)) # additional whitespace
+  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub(" $", "", x)) # end Whitespace
+  
+  # LOWER CASE
+  isear.data$SIT <- sapply(isear.data$SIT, tolower)
+  
+  # TM - STEMMING
+  isear.docs <- Corpus(VectorSource(isear.data$SIT))
+  isear.docs <- tm_map(isear.docs, removeNumbers) # Remove numbers
+  isear.docs <- tm_map(isear.docs, removeWords, stopwords("english")) # Remove english common stopwords e.g "the", "is", "of", etc
+  isear.docs <- tm_map(isear.docs, removePunctuation) # Remove punctuations
+  isear.docs <- tm_map(isear.docs, stripWhitespace) # Eliminate extra white spaces
+  
+  isear.docs <- tm_map(isear.docs, stemDocument) # Text stemming (reduces words to their root form)
+  isear.docs <- tm_map(isear.docs, removeWords, c("clintonemailcom", "stategov", "hrod")) # Remove additional stopwords
+  
+  # GET STEMMING SENTENCES BACK TO ISEAR.DATA
+  isear.data$SIT <- sapply(isear.docs, identity)
+  
+  return(isear.data)
+}
+
+
+
+
+
+
+
+
+# ========================= DATA FOR TRAINING ML MODELS ====================================
+
+# CREATE THE BAG OF WORDS
+bag.of.words <- function(isear.data){
+  isear.docs <- Corpus(VectorSource(isear.data$SIT))
+  isear.dtm <- DocumentTermMatrix(isear.docs)
+  mat <- as.matrix(isear.dtm)
+  return(mat)
+}
 
 # GET TRAIN, TEST AND VALIDATION DATA TURNED INTO THE BAG OF WORDS
 get.bagOfWords.allPartData <- function(path = ""){
@@ -35,22 +97,10 @@ get.bagOfWords.allPartData <- function(path = ""){
   
   # PARTITION THE DATA
   data <- partition.data( c(0.8, 1), bagOfWords )
-    
+  
   return(data)
 }
 
-# Get All Data without noise ---> EMOT label -> emotion tag
-# SIT label -> Text
-getPreproc.Data.ISEAR <- function(path = ""){
-  
-  if (path == "") {
-    path <- paste(getwd(),"isear.csv", sep = "/")
-  }
-  data.raw <- getData.ISEAR(path)
-  data.prep <- preproccess.ISEAR(data.raw)
-  
-  return(data.prep)
-}
 
 # PARTITIONED DATA TO TRAIN
 partition.data <- function(values, data){
@@ -64,80 +114,9 @@ partition.data <- function(values, data){
   return(data_part)
 }
 
-# CREATE THE BAG OF WORDS
-bag.of.words <- function(isear.data){
-  isear.docs <- Corpus(VectorSource(isear.data$SIT))
-  isear.dtm <- DocumentTermMatrix(isear.docs)
-  mat <- as.matrix(isear.dtm)
-  return(mat)
-}
-
-# DATA TO OBTAIN
-
-getData.ISEAR <- function(path){
-  
-    # READ & GET IMPORTANT ATTRIBUTES
-    data <- read.csv(path, header = TRUE, sep = "|", stringsAsFactors = FALSE)
-    data <- data[c("COUN","SUBJ","AGE","EMOT","Field1","SIT","LONG","INTS")]
-    
-    # EMOTIONS TAGGED
-    data <- data[(data$Field1 != ""),]
-    data$EMOT <- factor(data$EMOT)
-    levels(data$EMOT) <- list("joy" = "1", "fear" = "2", "anger" = "3", "sadness" = "4", "disgust" = "5", "shame" = "6", "guilt" = "7")
-
-    return(data)
-}
-
-# DATA PRE_PROCESSING
-
-preproccess.ISEAR <- function(isear.data){
-  
-  # DELETE ALL NON-ALPHANUMERIC CHARACTERS & additional whitespace
-  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub("[^a-zA-Z0-9']", " ", x)) # non-alphanumeric characters
-  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub("\\s+", " ", x)) # additional whitespace
-  isear.data$SIT <- sapply(isear.data$SIT, function(x) gsub(" $", "", x)) # end Whitespace
-  
-  
-  # LOWER CASE
-  isear.data$SIT <- sapply(isear.data$SIT, tolower)
-  
-  
-  # TM - STEMMING
-  isear.docs <- Corpus(VectorSource(isear.data$SIT))
-  isear.docs <- tm_map(isear.docs, removeNumbers) # Remove numbers
-  isear.docs <- tm_map(isear.docs, removeWords, stopwords("english")) # Remove english common stopwords e.g "the", "is", "of", etc
-  isear.docs <- tm_map(isear.docs, removePunctuation) # Remove punctuations
-  isear.docs <- tm_map(isear.docs, stripWhitespace) # Eliminate extra white spaces
-  
-  #isear.docs <- tm_map(isear.docs, stemDocument) # Text stemming (reduces words to their root form)
-  isear.docs <- tm_map(isear.docs, removeWords, c("clintonemailcom", "stategov", "hrod")) # Remove additional stopwords
-  
-  # ========== UNCOMMENT THE NEXT LINES FOR WORDCLOUD WARN : SLOW COMP TIME =================
-  # FRECUENCY MATRIX OF WORDS
-  dtm <- TermDocumentMatrix(isear.docs)
-  isear.freq <- as.matrix(dtm)
-  isear.freq <- sort(rowSums(isear.freq),decreasing=TRUE)
-  isear.freq <- data.frame(word = names(isear.freq),freq=isear.freq)
-  
-  
-  # GENERATE WORDCLOUD
-  generate.wordcloud(isear.freq, "ISEARS's  words - Most Used")
-  # =========================================================================================
-  
-  # GET STEMMING SENTENCES BACK TO ISEAR.DATA
-  isear.data$SIT <- sapply(isear.docs, identity)
-  
-  return(isear.data)
-}
 
 
-generate.wordcloud <- function(data.freq, data.title = "Most Used Words"){
-  # Generate a wordcloud based on a data frame of frequencies
-  par(bg="grey30")
-  png(file="WordCloud.png",width=1000,height=700, bg="grey30")
-  write("Generating wordCloud .....", stdout())
-  wordcloud(data.freq$word, data.freq$freq, col=terrain.colors(length(data.freq$word), alpha=0.9), random.order=FALSE, rot.per=0.3 )
-  write("Done, Saved on Working Directory", stdout())
-  title(main = data.title, font.main = 1, col.main = "cornsilk3", cex.main = 1.5)
-  dev.off()
-}
+
+
+
+
