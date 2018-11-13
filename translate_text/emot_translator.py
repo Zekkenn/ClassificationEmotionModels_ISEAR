4860a8fe41749606e1d2d3405a27ae59dfbfcbcb
JEMurcia <jossie.murcia@hotmail.com>
2018-11-13 06:12:31 -0500

    Spanish datasets

---
+++ translate_text/emot_translator.py
@@ -0,0 +1,72 @@
+"""
+This Application translate ISEAR & SemEval 2007: 14 Task
+    From they original language (English) to Spanish with
+Google Cloud Translate API
+
+For more information, the documentation at
+https://cloud.google.com/translate/docs.
+"""
+
+from google.cloud import translate
+from tempfile import NamedTemporaryFile
+
+import xml.etree.ElementTree as ET
+import shutil
+import csv
+import six
+
+SEMEVAL_PATH_IN = r'C:\Users\Esteban\Documents\Google Cloud Projects\python-docs-samples\translate\cloud-client\SemEval_14\AffectiveText.test\affectivetext_test.xml'
+SEMEVAL_PATH_OUT = r'C:\Users\Esteban\Documents\Google Cloud Projects\python-docs-samples\translate\cloud-client\SemEval_14\AffectiveText.test.spanish\affectivetext_test.xml'
+
+ISEAR_PATH_IN = r'C:\Users\Esteban\Documents\Google Cloud Projects\python-docs-samples\translate\cloud-client\ISEAR\isear.csv'
+ISEAR_PATH_OUT = r'C:\Users\Esteban\Documents\Google Cloud Projects\python-docs-samples\translate\cloud-client\ISEAR\isear_spanish.csv'
+
+def translate_semEval(semEval_xml):
+    tree = ET.parse(semEval_xml)  
+    root = tree.getroot()
+
+    # Change sentences
+    for elem in root.iter('instance'):
+        elem.text = translate_text('es',elem.text)
+
+    tree.write(SEMEVAL_PATH_OUT)
+
+def translate_isear(isear_csv):
+
+    tempfile = NamedTemporaryFile(mode='w', encoding='utf-8', delete=False)
+    
+    with open(isear_csv, 'r') as isear, tempfile:
+        isear_reader = csv.DictReader(isear, delimiter='|')
+        isear_writer = csv.DictWriter(tempfile, fieldnames=isear_reader.fieldnames, delimiter='|', lineterminator='\n')
+        isear_writer.writeheader()
+
+        for row in isear_reader:
+            row['SIT'] = translate_text('es',row['SIT'])            
+            new_row = {key: value for key, value in row.items()
+                       if key in isear_writer.fieldnames}
+            isear_writer.writerow(new_row)
+            
+    shutil.move(tempfile.name, ISEAR_PATH_OUT)
+
+def translate_text(target, text):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(
+        text, target_language=target)
+
+    return(result['translatedText'])
+
+
+if __name__ == "__main__":
+    # translate_semEval(SEMEVAL_PATH_IN)
+    translate_isear(ISEAR_PATH_IN)
--- translate_text/snippets.py
+++ translate_text/snippets.py
@@ -16,6 +16,7 @@
 
 """This application demonstrates how to perform basic operations with the
 Google Cloud Translate API
+
 For more information, the documentation at
 https://cloud.google.com/translate/docs.
 """
@@ -56,6 +57,7 @@ def list_languages():
 def list_languages_with_target(target):
     # [START translate_list_language_names]
     """Lists all available languages and localizes them to the target language.
+
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
@@ -71,7 +73,9 @@ def list_languages_with_target(target):
 def translate_text_with_model(target, text, model=translate.NMT):
     # [START translate_text_with_model]
     """Translates text into the target language.
+
     Make sure your project is whitelisted.
+
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
@@ -95,6 +99,7 @@ def translate_text_with_model(target, text, model=translate.NMT):
 def translate_text(target, text):
     # [START translate_translate_text]
     """Translates text into the target language.
+
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
@@ -115,6 +120,7 @@ def translate_text(target, text):
     # [END translate_translate_text]
 
 
+'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -147,3 +153,4 @@ if __name__ == '__main__':
         list_languages_with_target(args.target)
     elif args.command == 'translate-text':
         translate_text(args.target, args.text)
+'''
